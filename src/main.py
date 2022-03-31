5abcd65b7e9bda786c49f3bff53885be9d5815be
Norberto <ndelacruzfalcon@gmail.com>
2022-03-31 20:26:05 +0100

    Start to scrap Fotocasa

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import selenium
+from selenium.webdriver.common.by import By
+from fcScrapper import fcScrapper
+
+URL = 'https://www.fotocasa.es/es/alquiler/viviendas/barcelona-capital/todas-las-zonas/l'
+PATH = 'webdriver/chromedriver.exe'
+
+scrapper = fcScrapper.FcScrapper(1, URL, PATH)
+scrapper.scrap_page_clicking()
+input()
+scrapper.close_browser()
+
